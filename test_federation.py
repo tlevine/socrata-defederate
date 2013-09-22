8499ffe8afe442a34671e96966ac07cf73ab8cff
Thomas Levine <occurrence@thomaslevine.com>
2013-09-22 17:37:30 +0000

    test type of output

--- test_federation.py
+++ test_federation.py
@@ -55,3 +55,18 @@ def test_parse_page():
     ]
     observed = federation.parse_page(html)
     n.assert_list_equal(observed, expected)
+
+def test_build_json():
+    observed = json.loads(federation.build_json())
+    expected_keys = {'edges', 'nodes'}
+    n.assert_equal(set(observed.keys()), expected_keys)
+    for k in expected_keys:
+        n.assert_equal(type(observed[k]), list)
+
+    for edge in observed['edges']:
+        n.assert_equal(type(edge), list)
+        for node in edge:
+            n.assert_equal(type(node), unicode)
+
+    for node in observed['nodes']:
+        n.assert_equal(type(node), unicode)
