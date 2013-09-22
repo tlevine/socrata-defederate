640fdd25beb053c4b76dae9ed525cfd89bdb36f4
Thomas Levine <occurrence@thomaslevine.com>
2013-09-22 17:17:39 +0000

    add data.gov

--- test_federation.py
+++ test_federation.py
@@ -5,18 +5,6 @@ import nose.tools as n
 
 import federation
 
-html = fromstring('''
-    <div class="browseFacets ">
-      <div class="facetSection clearfix federation_filter">
-          <h4 class="title">Federated Domains</h4>
-            <ul>
-                <li><a href="/browse/embed?federation_filter=162&utf8=%E2%9C%93" class=""><img class="customIcon" src="/api/domains/data.hawaii.gov/icons/smallIcon" alt="icon" />This site only</a></li>
-                <li><a href="/browse/embed?federation_filter=19&utf8=%E2%9C%93" class=""><img class="customIcon" src="/api/domains/explore.data.gov/icons/smallIcon" alt="icon" />explore.data.gov</a></li>
-            </ul>
-        </div>
-      </div>
-''')
-
 def check_parse_targets(filename):
     html = fromstring(open(filename).read())
     expected = json.load(open(filename.replace('.html', '.json')))
@@ -33,6 +21,17 @@ def check_parse_source(filename):
     n.assert_equal(observed, expected)
 
 def test_parse_targets_snippet():
+    html = fromstring('''
+    <div class="browseFacets ">
+      <div class="facetSection clearfix federation_filter">
+          <h4 class="title">Federated Domains</h4>
+            <ul>
+                <li><a href="/browse/embed?federation_filter=162&utf8=%E2%9C%93" class=""><img class="customIcon" src="/api/domains/data.hawaii.gov/icons/smallIcon" alt="icon" />This site only</a></li>
+                <li><a href="/browse/embed?federation_filter=19&utf8=%E2%9C%93" class=""><img class="customIcon" src="/api/domains/explore.data.gov/icons/smallIcon" alt="icon" />explore.data.gov</a></li>
+            </ul>
+        </div>
+      </div>
+''')
     observed = federation.parse_targets(html)
     expected = ['explore.data.gov']
     n.assert_list_equal(observed, expected)
@@ -46,3 +45,11 @@ def test_parse_source():
     for basename in os.listdir('fixtures'):
         if basename.endswith('.html'):
             yield check_parse_source, os.path.join('fixtures', basename)
+
+'''
+def test_parse_page():
+    html = fromstring(open(os.path.join('fixtures', 'explore.data.gov.html')).read())
+    icondata.cityofboston.gov
+    icondata.medicare.gov
+    iconinfo.samhsa.gov
+'''
