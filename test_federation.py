99682d5f89018bee98b52818e3c948f241b595a5
Thomas Levine <occurrence@thomaslevine.com>
2013-09-22 16:38:35 +0000

    parse

---
+++ test_federation.py
@@ -0,0 +1,11 @@
+html = fromstring('''
+    <div class="browseFacets ">
+      <div class="facetSection clearfix federation_filter">
+          <h4 class="title">Federated Domains</h4>
+            <ul>
+                <li><a href="/browse/embed?federation_filter=162&utf8=%E2%9C%93" class=""><img class="customIcon" src="/api/domains/data.hawaii.gov/icons/smallIcon" alt="icon" />This site only</a></li>
+                <li><a href="/browse/embed?federation_filter=19&utf8=%E2%9C%93" class=""><img class="customIcon" src="/api/domains/explore.data.gov/icons/smallIcon" alt="icon" />explore.data.gov</a></li>
+            </ul>
+        </div>
+      </div>
+''')
