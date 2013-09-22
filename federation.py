599a57634667f8a9120b0b2ee60b7efdb114fa2e
Thomas Levine <occurrence@thomaslevine.com>
2013-09-22 17:08:54 +0000

    parsing targets

--- federation.py
+++ federation.py
@@ -4,7 +4,9 @@ def parse_page():
     return []
 
 def parse_targets(html):
-    return []
+    srcs = html.xpath('//div[@class="browseFacets"]/div/h4/ul/li/a/img/@src')
+    print srcs
+    return [re.match(r'/api/domains/([^/]+)//icons/smallIcon', src).group(1) for src in srcs]
 
 def parse_source(html):
     url = html.xpath('//meta[@property="og:url"]/@content')[0]
