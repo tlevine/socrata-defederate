f109894bf5e6aef4de2bf651e6b9627d2a7a0c0f
Thomas Levine <occurrence@thomaslevine.com>
2013-09-23 12:59:36 +0000

    dedupe works

--- test_dedupe.py
+++ test_dedupe.py
@@ -2,12 +2,11 @@ import nose.tools as n
 
 import dedupe
 
-@n.nottest
 def test_dedupe():
     dcat1 = [{'identifier': 'a', 'portal': 1}, {'identifier': 'b', 'portal': 1}, {'identifier': 'c', 'portal': 1}]
     dcat2 = [{'identifier': 'g', 'portal': 2}, {'identifier': 'h', 'portal': 2}, {'identifier': 'c', 'portal': 2}]
     edges = [('portal1', 'portal2')]
-    observed = dedupe(('portal1', dcat1), ('portal2', dcat2), edges)
+    observed = dedupe.dedupe(('portal1', dcat1), ('portal2', dcat2), edges)
 
     # Sort by identifier
     expected = [
