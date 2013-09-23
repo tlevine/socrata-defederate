323adb339e498dced762e269588df84aa4f26f4d
Thomas Levine <occurrence@thomaslevine.com>
2013-09-23 12:32:54 +0000

    unicode

--- federation.py
+++ federation.py
@@ -10,7 +10,7 @@ def parse_page(html):
 
 def parse_targets(html):
     srcs = html.xpath('//h4[text()="Federated Domains"]/following-sibling::ul[position()=1]/li/a/img/@src')
-    return [re.match(r'/api/domains/([^/]+)/icons/smallIcon', src).group(1) for src in srcs[1:]]
+    return [unicode(re.match(r'/api/domains/([^/]+)/icons/smallIcon', src).group(1)) for src in srcs[1:]]
 
 def parse_source(html):
     url = html.xpath('//meta[@property="og:url"]/@content')[0]
