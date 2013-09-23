a120d623f2b47581d6709a88f44c91973e75695b
Thomas Levine <occurrence@thomaslevine.com>
2013-09-23 12:55:23 +0000

    dedupe

--- dedupe.py
+++ dedupe.py
@@ -1,27 +1,43 @@
 #!/usr/bin/env python
+from collections import OrderedDict
+
 from federation import build_network
 
-def reducer(a, b, edges = None):
+def dedupe(a, b, edges):
     '''
     Args:
         Two tuples of (portal, dcat list)
     Returns:
-        One list of dcat
+        One list of dcat, sorted by identifier
 
     This deduplicates and combines of dcat based on the edges of the federation graph.
     '''
-    if edges == None:
-        edges = build_network()['edges']
+    a_portal, a_list = a
+    b_portal, b_list = b
 
+    a_dict = OrderedDict(zip([item['identifier'] for item in a_list], a_list))
+    b_dict = OrderedDict(zip([item['identifier'] for item in b_list], b_list))
 
-    return l
+    winner = winning_portal(a_portal, b_portal, edges)
+
+    if winner == a_portal:
+        b_dict.update(a_dict)
+        return b_dict.values()
+    else:
+        a_dict.update(b_dict)
+        return a_dict.values()
+
+def winning_portal(a, b, edges):
+    return a
 
 def load():
     '''
     Returns an iterable of dcat lists
     '''
     for data_json in os.listdir(os.path.join('socrata-catalog', 'catalogs')):
-        yield json.load(open(os.path.join('catalogs', data_json)))[1:]
+        portal = data_json.replace('.json', '')
+        yield data_json, json.load(open(os.path.join('socrata-catalogs', 'catalogs', data_json)))[1:]
 
 def main():
-    reduce(lambda a,b:a+b,load())
+    edges = build_network()['edges']
+    print load().next()
