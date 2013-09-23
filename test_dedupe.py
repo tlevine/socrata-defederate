faf58e8715c69cda256e7bff3187ddb7bb3cbc4d
Thomas Levine <occurrence@thomaslevine.com>
2013-09-23 12:47:36 +0000

    test dedupe

---
+++ test_dedupe.py
@@ -0,0 +1,13 @@
+def test_dedupe():
+    dcat1 = [{'identifier': 'a', 'portal': 1}, {'identifier': 'b', 'portal': 1}, {'identifier': 'c', 'portal': 1}]
+    dcat2 = [{'identifier': 'g', 'portal': 2}, {'identifier': 'h', 'portal': 2}, {'identifier': 'c', 'portal': 2}]
+    edges = [('portal1', 'portal2')]
+    observed = dedupe(('portal1', dcat1), ('portal2', dcat2), edges)
+
+    # Sort by identifier
+    expected = [
+        {'identifier': 'a', 'portal': 1}, {'identifier': 'b', 'portal': 1},
+        {'identifier': 'c', 'portal': 2},
+        {'identifier': 'g', 'portal': 2}, {'identifier': 'h', 'portal': 2},
+    ]
+    n.assert_list_equal(observed, expected)
