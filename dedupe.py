9c5d0be18cb1f62c5872e76dc3f6e25f84cc8fa6
Thomas Levine <occurrence@thomaslevine.com>
2013-09-23 12:42:50 +0000

    oeu

--- dedupe.py
+++ dedupe.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 from federation import build_network
 
-def filterer(l, edges = None):
+def reducer(a, b, edges = None):
     '''
-    Takes a list of dcat and filters it
+    Args:
+        Two tuples of (portal, dcat list)
+    Returns:
+        One list of dcat
+
+    This deduplicates and combines of dcat based on the edges of the federation graph.
     '''
     if edges == None:
         edges = build_network()['edges']
@@ -15,7 +20,7 @@ def load():
     '''
     Returns an iterable of dcat lists
     '''
-    for data_json in os.listdir('catalogs'):
+    for data_json in os.listdir(os.path.join('socrata-catalog', 'catalogs')):
         yield json.load(open(os.path.join('catalogs', data_json)))[1:]
 
 def main():
